9f1d822b60c64884515a785ad25466c1f55f26f8
mubarekmuhammed <mubamuha8648@gmail.com>
2022-03-23 03:51:07 +0300

    pointer,arrays and string commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,27 @@
 #include "main.h"
 
 /**
- * puts2 - prints one char out of 2
- * @str: type
+ * puts2 - print character of a string
+ *      in an array starting from the first
+ *      character followed by the rest of the
+ *      even index array
  *
- */
+ * @str: string input array pointer
+ *
+ * Return: nothing
+*/
 
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] >= 0; i++)
 	{
-		i++;
-	}
+		if (str[i] == '\0')
+			break;
 
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
